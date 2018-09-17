8ac817864c214592000cd5eea2212eb6900c981d
2319484461@qq.com <2319484461@qq.com>
2018-09-17 10:19:48 +0800

    首次上传

---
+++ KHRefresh/KHRefresh.framework/Headers/KHRefresh.h
@@ -0,0 +1,20 @@
+//
+//  KHRefresh.h
+//  KHRefresh
+//
+//  Created by 王高峰 on 2018/9/17.
+//  Copyright © 2018年 KH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "UIScrollView+KHRefesh.h"
+
+//! Project version number for KHRefresh.
+FOUNDATION_EXPORT double KHRefreshVersionNumber;
+
+//! Project version string for KHRefresh.
+FOUNDATION_EXPORT const unsigned char KHRefreshVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KHRefresh/PublicHeader.h>
+
+
